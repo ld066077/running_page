7cffff5ec3b68f54391307cfa2c4bf0a802cb510
ld066077 <157603690+ld066077@users.noreply.github.com>
2024-04-25 06:56:45 +0800

    new appe

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,14 +10,18 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running record',
+  siteTitle: 'imyxl的跑步记录（数据源自keep）',
   siteUrl: 'https://run.imyxl.com',
   logo: '',
   description: 'Personal site and blog',
   navLinks: [
     {
-      name: 'Blog',
+      name: '我的博客',
       url: 'https://imyxl.com',
+    },
+    {
+      name: '我的github仓库',
+      url: 'https://github.com/ld066077',
     }
   ],
 };
